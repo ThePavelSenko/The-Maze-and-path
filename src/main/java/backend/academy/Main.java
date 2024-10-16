b3549887c6f79839626f04c09eb64003b34202e9
Pasha Senko <pashasenko19@gmail.com>
2024-10-16 10:45:46 +0500

    feat(main, utils): add new logic in the main class and implement new fields in utils class for using them in main.

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,12 +1,58 @@
 package backend.academy;
 
+import java.util.List;
+import java.util.Scanner;
 import lombok.experimental.UtilityClass;
+import static backend.academy.Utils.GOODBYE_MESSAGE;
+import static backend.academy.Utils.HELLO_MESSAGE;
+import static backend.academy.Utils.IF_EASY_MESSAGE;
+import static backend.academy.Utils.IF_HARD_MESSAGE;
+import static backend.academy.Utils.OUT;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
-        final int SIZE = 10;
-        KruskalMaze maze = new KruskalMaze(SIZE, SIZE);
-        maze.printMaze();
+        final int ifEasySize = 5;
+        final int ifHardSize = 10;
+        AbstractGraphMaze maze;
+        MazePathFinder pathFinder;
+        try (Scanner scanner = new Scanner(System.in)) {
+            OUT.println(HELLO_MESSAGE);
+            String response = scanner.nextLine();
+            switch (response) {
+                case "easy":
+                    maze = new PrimMaze(ifEasySize, ifEasySize);
+                    pathFinder = new BFS(maze);
+                    OUT.println(IF_EASY_MESSAGE);
+                    break;
+                case "hard":
+                    maze = new KruskalMaze(ifHardSize, ifHardSize);
+                    pathFinder = new AStar(maze);
+                    OUT.println(IF_HARD_MESSAGE);
+                    break;
+                default:
+                    throw new IllegalStateException(response);
+            }
+            maze.generateMaze();
+            List<String> outputMaze = maze.assembleMaze(maze.mazeEdges(), maze.height(), maze.width());
+            maze.printMaze(outputMaze);
+            String showPath = scanner.nextLine();
+            switch (showPath) {
+                case "Yes":
+                    pathFinder.findPath();
+                    pathFinder.printPath(outputMaze);
+                    OUT.println(GOODBYE_MESSAGE);
+                    break;
+                case "No":
+                    OUT.println(GOODBYE_MESSAGE);
+                    break;
+                default:
+                    throw new IllegalStateException(showPath);
+            }
+        } catch (IllegalStateException e) {
+            OUT.println("You must enter correct value! Please try again!");
+        }
     }
 }
+
+
--- src/main/java/backend/academy/Utils.java
+++ src/main/java/backend/academy/Utils.java
@@ -1,5 +1,6 @@
 package backend.academy;
 
+import java.io.PrintStream;
 import java.security.SecureRandom;
 import java.util.List;
 import lombok.AccessLevel;
@@ -7,6 +8,25 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Utils {
+    public final static PrintStream OUT = System.out;
+    public final static String HELLO_MESSAGE = """
+                Welcome to the maze generation! If you want to generate a maze enter easy/hard!
+                easy: will be generated a maze with size 5*5 and alone path. (will be use Prim algorithm)
+                hard: will be generated a maze with size 10*10 and 2 and more path and will be create a special cells.\
+                 (will be use Kruskal algorithm)""";
+
+    public final static String GOODBYE_MESSAGE = "Goodbye! See you soon!";
+
+    public final static String IF_EASY_MESSAGE = """
+        Try find the path in the maze. If you want to see this path\
+         - enter "Yes" or "No" if dont want.""";
+
+    public final static String IF_HARD_MESSAGE = """
+        Try find the best path in the maze
+        if '$' takes 1 point and '~' takes away 1 point. If you want to see this path\
+         - enter "Yes" or "No" if dont want.""";
+
+
     public static <T> T getRandomValue(List<T> list) {
         SecureRandom random = new SecureRandom();
         int randomIndex = random.nextInt(list.size());
